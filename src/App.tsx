f5a3366145cd3150eb7ea115be233db7704e36bc
Tharindu Lakshan <145644501+DilumiGimansha@users.noreply.github.com>
2026-06-23 23:21:57 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,8 @@ import AOS from 'aos';
 import 'aos/dist/aos.css';
 import { useEffect } from "react";
 
-pdfjs.GlobalWorkerOptions.workerSrc = `https://cdn.jsdelivr.net/npm/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
+// pdfjs.GlobalWorkerOptions.workerSrc = `https://cdn.jsdelivr.net/npm/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
+pdfjs.GlobalWorkerOptions.workerSrc = `https://unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`;
 
 function App() {
   useEffect(()=>{
